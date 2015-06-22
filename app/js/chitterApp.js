0983086d189a3aa965f2ee26405ae2b66f938e8a
Kevin Lanzon <kevinlanzon@gmail.com>
2015-06-22 12:04:12 +0100

    adds data binding and filtering

--- app/js/chitterApp.js
+++ app/js/chitterApp.js
@@ -1,5 +1,12 @@
 var app = angular.module('chitterApp', []);
 
 app.controller('mainController', function($scope) {
+  $scope.posts = [];
+  $scope.newPost = {created_by: '', text: '', created_at: ''};
 
+  $scope.post = function() {
+    $scope.newPost.created_at = Date.now();
+    $scope.posts.push($scope.newPost);
+    $scope.newPost = {created_by: '', text: '', created_at: ''};
+  };
 });
