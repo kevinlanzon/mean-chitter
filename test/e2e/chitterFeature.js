57d43542bd9e6a929c5758d7d0c0ff861965c6cb
Kevin Lanzon <kevinlanzon@gmail.com>
2015-06-23 19:32:26 +0100

    e2e test for adding a post passing

--- test/e2e/chitterFeature.js
+++ test/e2e/chitterFeature.js
@@ -1,7 +1,21 @@
 describe('Homepage', function() {
-  it('should have a title', function() {
-    browser.get('/app/main.html');
 
+  beforeEach(function() {
+    browser.get('/app/main.html');
+    addName = element(by.id('name'));
+    addMessage = element(by.id('message'));
+    submitMessage = element(by.id('submit-chirp'));
+  });
+
+  it('should have a title', function() {
     expect(browser.getTitle()).toEqual('Chitter');
+  });
+
+  it('can add a user name and post a chirp', function() {
+    addName.sendKeys('kev');
+    addMessage.sendKeys('This is my first post!');
+    submitMessage.click();
+    expect(element(by.id('posted-by')).getText()).toEqual('Posted by @kev');
+    expect(element(by.id('post')).getText()).toEqual('This is my first post!');
   });
 });
