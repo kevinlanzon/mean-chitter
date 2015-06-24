9cd727adef1023f2d67066430296bfd673d2f7c7
Kevin Lanzon <kevinlanzon@gmail.com>
2015-06-24 14:26:18 +0100

    adds register and login pages

--- app/js/chitterApp.js
+++ app/js/chitterApp.js
@@ -10,3 +10,18 @@ app.controller('mainController', function($scope) {
     $scope.newPost = {created_by: '', text: '', created_at: ''};
   };
 });
+
+app.controller('authController', function($scope) {
+  $scope.user = {username: '', password: ''};
+  $scope.error_message = '';
+
+  $scope.login = function() {
+    //placeholder until authentication is implemented
+    $scope.error_message = 'login request for ' + $scope.user.username;
+  };
+
+  $scope.register = function() {
+    //placeholder until authentication is implemented
+    $scope.error_message = 'registeration request for ' + $scope.user.username;
+  };
+});
