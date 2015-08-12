8d38896fa2aa56f71ad5dc4d3556be25c3d3c8b3
Kevin Lanzon <kevinlanzon@gmail.com>
2015-08-12 17:43:51 +0100

    sets up routes and user authentication

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var passport = require('passport');
-var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
+var passport = require('passport');
 var api = require('./routes/api');
-// var authenticate = require('./routes/authenticate');
+var authenticate = require('./routes/authenticate')(passport);
 
 var app = express();
 
@@ -20,7 +19,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(session({
-  secret: 'super secret'
+  secret: 'keyboard cat'
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,39 +28,42 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use('/auth', authenticate);
 app.use('/api', api);
-// app.use('/auth', authenticate);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
+
+//// Initialize Passport
+var initPassport = require('./passport-init');
+initPassport(passport);
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
 
 module.exports = app;
--- passport-init.js
+++ passport-init.js
@@ -1,70 +1,72 @@
-var User = require('./models/models');
-var mongoose = require('mongoose');
-var User = mongoose.model('User');
 var LocalStrategy   = require('passport-local').Strategy;
 var bCrypt = require('bcrypt-nodejs');
-users = {};
+//temporary data store
+var users = {};
 module.exports = function(passport){
 
-    // Passport needs to be able to serialize and deserialize users to support persistent login sessions
-    passport.serializeUser(function(user, done) {
-        console.log('serializing user:',user.username);
-        //return the unique id for the user
-        done(null, user.username);
-    });
+  // Passport needs to be able to serialize and deserialize users to support persistent login sessions
+  // Passport needs to be able to serialize and deserialize users to support persistent login sessions
+  passport.serializeUser(function(user, done) {
+    console.log('serializing user:',user.username);
+    //return the unique id for the user
+    done(null, user.username);
+  });
 
-    //Desieralize user will call with the unique id provided by serializeuser
-    passport.deserializeUser(function(username, done) {
+  //Desieralize user will call with the unique id provided by serializeuser
+  passport.deserializeUser(function(username, done) {
 
+    return done(null, users[username]);
+
+  });
+
+  passport.use('login', new LocalStrategy({
+      passReqToCallback : true
+    },
+    function(req, username, password, done) {
+
+      if(users[username]){
+        console.log('User Not Found with username '+username);
+        return done(null, false);
+      }
+
+      if(isValidPassword(users[username], password)){
+        //sucessfully authenticated
         return done(null, users[username]);
+      }
+      else{
+        console.log('Invalid password '+username);
+        return done(null, false)
+      }
+    }
+  ));
 
-    });
+  passport.use('signup', new LocalStrategy({
+      passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, username, password, done) {
 
-    passport.use('login', new LocalStrategy({
-            passReqToCallback : true
-        },
-        function(req, username, password, done) {
-            //check if user exists
-            if(users[username]){
-                console.log('User Not Found with username '+username);
-                return done(null, false);
-            }
-            //check if password is correct
-            if(isValidPassword(users[username], password)){
-                //sucessfully authenticated
-                return done(null, users[username]);
-            }
-            else{
-                console.log('Invalid password '+username);
-                return done(null, false)
-            }
-        }
-    ));
+      if (users[username]){
+        console.log('User already exists with username: ' + username);
+        return done(null, false);
+      }
 
-    passport.use('signup', new LocalStrategy({
-            passReqToCallback : true // allows us to pass back the entire request to the callback
-        },
-        function(req, username, password, done) {
-            //check if user exists
-            if (users[username]){
-                console.log('User already exists with username: ' + username);
-                return done(null, false);
-            }
+      //store user in memory
+      users[username] = {
+        username: username,
+        password: createHash(password)
+      }
 
-            users[username] = {
-                username: username,
-                password: createHash(password)
-            }
+      console.log(users[username].username + ' Registration successful');
+      return done(null, users[username]);
+    })
+  );
 
-            console.log(users[username].username + ' Registration successful');
-            return done(null, users[username]);
-        })
-    );
+  var isValidPassword = function(user, password){
+    return bCrypt.compareSync(password, user.password);
+  };
+  // Generates hash using bCrypt
+  var createHash = function(password){
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+  };
 
-    var isValidPassword = function(user, password){
-        return bCrypt.compareSync(password, user.password);
-    };
-    // Generates hash using bCrypt
-    var createHash = function(password){
-        return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
-    };
+};
