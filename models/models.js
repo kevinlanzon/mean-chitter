bc4b971c0bf7ce14d7afe9e1027c7ebc3b0f0d70
Kevin Lanzon <kevinlanzon@gmail.com>
2015-08-13 12:34:25 +0100

    changes object id for posts in schema

--- models/models.js
+++ models/models.js
@@ -1,19 +1,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var userSchema = new mongoose.Schema({
-  username: String,
-  password: String, //hash created from password
-  created_at: {type: Date, default: Date.now}
-});
-
 //post schema to represent our entries when a user 'Chirps' a message
 var postSchema = new mongoose.Schema({
-  created_by: { type: Schema.ObjectId, ref: 'User' },
+  created_by: String,   //should be changed to ObjectId, ref "User"
   created_at: {type: Date, default: Date.now},
   text: String
 });
 
+var userSchema = new mongoose.Schema({
+  username: String,
+  password: String, //hash created from password
+  created_at: {type: Date, default: Date.now}
+})
+
 //register a User and Post model with mongoose
-mongoose.model('User', userSchema);
 mongoose.model('Post', postSchema);
+mongoose.model('User', userSchema);
